7092a9a94780281b8441d23e7c22a78720c25f55
MythicalMichael <michael.chizhevskiy@hotmail.com>
2018-02-03 22:43:01 +0100

    second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,10 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { HttpModule } from "@angular/http";
+import { RouterModule, Routes } from "@angular/router";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+// Animations ?
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 
 import { AppComponent } from './app.component';
@@ -10,7 +15,10 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
